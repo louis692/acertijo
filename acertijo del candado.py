fb62c0263d6e70d5641c00c04b7b59cf6ec1ce83
louis692 <83940107+louis692@users.noreply.github.com>
2025-06-15 23:29:41 -0500

    Add files via upload

---
+++ acertijo del candado.py
@@ -0,0 +1,71 @@
+print()
+print(" PISTAS DISPONIBLES:")
+print("-" * 60)
+print("Fila 1 (3-2-1): Uno es correcto y está bien puesto")
+print("Fila 2 (5-6-2): Ninguno es correcto")
+print("Fila 3 (8-3-0): Dos son correctos pero no están bien puestos")
+print("Fila 4 (1-8-5): Dos son correctos pero no están bien puestos")
+print("-" * 60 + "\n")
+print()
+print()
+
+while True:
+    try:
+        numero_1 = int(input("Por favor, ingresa un número del 0 al 9: "))
+        if 0 <= numero_1 <= 9:    
+            print("Has ingresado el número válido:", numero_1)
+            if numero_1 == 8:
+                print("el primer numero es correcto")
+            elif numero_1 == 1:
+                print("el numero pertenece a la combinacion")
+            elif numero_1 == 3:    
+                print("el numero pertenece a la combinacion")
+            else:
+                 print("numero incorrecto")
+            break  
+        else:
+            print("Error: El número debe estar entre 0 y 9. Inténtalo de nuevo.")
+    except ValueError:
+        print("Error: Entrada no válida. Por favor, ingresa un número entero.")
+
+
+while True:
+    try:
+        numero_2 = int(input("Por favor, ingresa un número del 0 al 9: "))
+        if 0 <= numero_2 <= 9:
+            print("Has ingresado el número válido:", numero_2)
+            if numero_2 == 1:
+                print("el segundo numero es correcto")
+            elif numero_2 == 8:   
+                print("el numero pertenece a la combinacion") 
+            elif numero_2 == 3:
+             print("el numero pertenece a la combinacion")
+            else:
+                 print("numero incorrecto")
+            break  
+        else:
+            print("Error: El número debe estar entre 0 y 9. Inténtalo de nuevo.")
+    except ValueError:
+        print("Error: Entrada no válida. Por favor, ingresa un número entero.")
+
+while True:
+    try:
+        numero_3 = int(input("Por favor, ingresa un número del 0 al 9: "))
+        if 0 <= numero_3 <= 9:
+            print("Has ingresado el número válido:", numero_3)
+            if numero_3 == 3:
+                print("el tercer numero es correcto")
+            elif numero_3 == 8:
+                print("el numero pertenece a la combinacion")
+            elif numero_3 == 1:
+                print("el numero pertenece a la combinacion")  
+            else:
+                 print("numero incorrecto")         
+            break  
+        else:
+            print("Error: El número debe estar entre 0 y 9. Inténtalo de nuevo.")
+    except ValueError:
+        print("Error: Entrada no válida. Por favor, ingresa un número entero.")
+
+        
+        
